aa94b74ebb6b94ad2e01405a7dc98330f72061ce
Aravind Koneru <aravind.b.koneru@gmail.com>
2021-01-07 19:06:40 -0500

    added calculator project

---
+++ week10/tues/notes.py
@@ -0,0 +1,25 @@
+def do_something(a):
+    if a > 10:
+        return True
+    elif a < 0:
+        return 12
+    else:
+        return "less than 10 greater than 0"
+
+def do_other(a):
+    b = a + 1
+
+def do_test1(a):
+    if a > 10:
+        return True
+    return False
+
+def do_test2(a):
+    if a > 10:
+        return True
+    return None
+
+def do_test3(a):
+    if a > 10:
+        return True
+
